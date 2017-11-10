e68d89a17d0befda97d881709e9e2a214d236f63
Hubert Arnoux <hubert.arnoux@gmail.com>
2017-11-10 15:46:29 +0000

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
 
+  resources :users, only: [:show]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
